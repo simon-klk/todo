9780353fe6ca896e3720f991be4f05ffa9408942
Simon <simon.kalka@outlook.de>
2024-12-19 13:12:14 +0100

    Anischts änderung
    - Ansicht auf Dark Mode und Blau gesetzt

--- main_gui.py
+++ main_gui.py
@@ -1,10 +1,15 @@
 from todo import *
 import customtkinter as tk
 
+# Set the appearance mode to dark
+tk.set_appearance_mode("dark")
+tk.set_default_color_theme("blue")
 
 main = tk.CTk()
 main.title("To-Do List")
 main.geometry("400x400")
+
+
 
 
 main.mainloop()
