25b3009bad72f529ab2900b97a0b433e6d6372ec
Simon <simon.kalka@outlook.de>
2024-12-19 13:25:45 +0100

    Update Main und todo
    Änderung in Main_gui:
    - Feste Größe
    - Erstellung der Auswahlleiste oben
    Änderung todo.py:
    - Klasse und Init-Datei für Listen

--- main_gui.py
+++ main_gui.py
@@ -8,8 +8,16 @@ tk.set_default_color_theme("blue")
 main = tk.CTk()
 main.title("To-Do List")
 main.geometry("900x600")
+main.resizable(False, False)
 
 
+"""Leiste auswahl liste - oben"""
+
+# Erstellen der Leiste oben
+top_frame = tk.CTkFrame(main, width=892, height=60)
+top_frame.place(x=5, y=5)
+
+# Rechter Button für neue Liste
 
 
 main.mainloop()
