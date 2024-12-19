40ad3713287dda6bf3358c12bde18a2b8ad45fee
Simon <simon.kalka@outlook.de>
2024-12-19 14:46:09 +0100

    small change

--- todo_frame.py
+++ todo_frame.py
@@ -4,6 +4,6 @@ class ToDoFrame(tk.CTkFrame):
     def __init__(self, master, x, y, todo):
         super().__init__(master, width=150, height=150)
         self.place(x=5, y=70)
-        self.label = tk.CTkLabel(self, text=todo, font=12, width=100, height=40)
+        self.label = tk.CTkLabel(self, text=todo, width=100, height=40)
         self.label.place(x=x, y=y)
         
