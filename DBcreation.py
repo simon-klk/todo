059b024fa89fac2e4f3141c3831bb4541ca6f59b
Jayson Dina Mukueno <jayson.dinamukueno@kuka.com>
2024-12-19 14:16:44 +0100

    Items Tabelle erstellen

--- DBcreation.py
+++ DBcreation.py
@@ -4,13 +4,43 @@ connection = sqlite3.connect("Listen.db")
 cursor = connection.cursor()
 
 # Create Table that holds the ToDo-Lists
-
-list_id = "list_ID int NOT NULL AUTO_INCREMENT"
+list_id = "list_ID INTEGER PRIMARY KEY AUTOINCREMENT"
 list_name = "list_name varchar(20)"
 list_elem = "list_elem int"
 list_beschreibung = "list_beschreibung varchar(200)"
 list_erstelldatum = "list_erstelldatum date"
-list_setPrimary = "PRIMARY KEY (list_ID)"
 
-lists_string = f"CREATE TABLE IF NOT EXISTS listen(  {list_id},{list_name},{list_elem},{list_beschreibung},{list_erstelldatum},{list_setPrimary}  );"
-cursor.execute(lists_string)
+
+lists_string = f"""
+        CREATE TABLE IF NOT EXISTS listen(  
+        {list_id},
+        {list_name},
+        {list_elem},
+        {list_beschreibung},
+        {list_erstelldatum}  
+        );
+        """
+
+
+
+# Create Table that holds the Lists' items
+item_id = "item_ID INTEGER PRIMARY KEY AUTOINCREMENT"
+list_id = "list_ID INTEGER, FOREIGN KEY(list_ID) REFERENCES listen(list_ID)"
+item_text = "item_text varchar(20)"
+item_beschreibung = "item_beschreibung varchar(200)"
+item_datum = "item_datum date"
+item_enddatum = "item_end_datum date" 
+
+
+items_string = f"""
+        CREATE TABLE IF NOT EXISTS items(
+        {item_id},
+        {item_text},
+        {item_beschreibung},
+        {item_datum},
+        {item_enddatum},
+        {list_id}
+        );
+        """
+
+cursor.execute(items_string)
