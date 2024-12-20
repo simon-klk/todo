11fb9d4c706950c46b25d5b5c76a0fdfe4ec324b
Jayson Dina Mukueno <jayson.dinamukueno@kuka.com>
2024-12-20 08:15:25 +0100

    added class to file

--- DBcreation.py
+++ DBcreation.py
@@ -1,66 +1,67 @@
 import sqlite3
 
+class sqliteDB:
 
-def create_DB_and_Tables():
-    connection = sqlite3.connect("Listen.db")
-    cursor = connection.cursor()
+    def __init__(self, connection):     #connection = sqlite3.connect("Listen.db")
+        self.connection = connection  
+        self.cursor = self.connection.cursor()  
 
-    # Create Table that holds the ToDo-Lists
-    list_id = "list_ID INTEGER PRIMARY KEY AUTOINCREMENT"
-    list_name = "list_name varchar(20)"
-    list_elem = "list_elem int"     #Number of items inside LISTE
-    list_beschreibung = "list_beschreibung varchar(200)"
-    list_erstelldatum = "list_erstelldatum date"
+    def create_DB_and_Tables(self):
 
-
-    lists_string = f"""
-            CREATE TABLE IF NOT EXISTS listen(  
-            {list_id},
-            {list_name},
-            {list_elem},
-            {list_beschreibung},
-            {list_erstelldatum}  
-            );
-            """
+        # Create Table that holds the ToDo-Lists
+        list_id = "list_ID INTEGER PRIMARY KEY AUTOINCREMENT"
+        list_name = "list_name varchar(20)"
+        list_elem = "list_elem int"     #Number of items inside LISTE
+        list_beschreibung = "list_beschreibung varchar(200)"
+        list_erstelldatum = "list_erstelldatum date"
 
 
+        lists_string = f"""
+                CREATE TABLE IF NOT EXISTS listen(  
+                {list_id},
+                {list_name},
+                {list_elem},
+                {list_beschreibung},
+                {list_erstelldatum}  
+                );
+                """
 
-    # Create Table that holds the Lists' items
-    item_id = "item_ID INTEGER PRIMARY KEY AUTOINCREMENT"
-    list_id = "list_ID INTEGER, FOREIGN KEY(list_ID) REFERENCES listen(list_ID)"
-    item_text = "item_text varchar(20)"
-    item_beschreibung = "item_beschreibung varchar(200)"
-    item_datum = "item_datum date"
-    item_enddatum = "item_end_datum date" 
 
 
-    items_string = f"""
-            CREATE TABLE IF NOT EXISTS items(
-            {item_id},
-            {item_text},
-            {item_beschreibung},
-            {item_datum},
-            {item_enddatum},
-            {list_id}
-            );
-            """
+        # Create Table that holds the Lists' items
+        item_id = "item_ID INTEGER PRIMARY KEY AUTOINCREMENT"
+        list_id = "list_ID INTEGER, FOREIGN KEY(list_ID) REFERENCES listen(list_ID)"
+        item_text = "item_text varchar(20)"
+        item_beschreibung = "item_beschreibung varchar(200)"
+        item_datum = "item_datum date"
+        item_enddatum = "item_end_datum date" 
 
-    cursor.execute(items_string)
 
-    cursor.close()
-    connection.close()
+        items_string = f"""
+                CREATE TABLE IF NOT EXISTS items(
+                {item_id},
+                {item_text},
+                {item_beschreibung},
+                {item_datum},
+                {item_enddatum},
+                {list_id}
+                );
+                """
 
-# Benutze Commit nach jeder Änderung (Update, Insert into, Delete)
-def commit():
-    connection = sqlite3.connect("Listen.db")
-    connection.commit()
-    connection.close()
+        self.cursor.execute(items_string)
 
-# Benutze Rollback um Änderungen rückgängig zu machen       (NOCH NICHT GETESTET)
-def rollback():
-    connection = sqlite3.connect("Listen.db")
-    connection.rollback()
-    connection.close()
+    # Benutze Commit nach jeder Änderung (Update, Insert into, Delete)
+    def commit(self):
+        self.connection.commit()
+
+    # Benutze Rollback um Änderungen rückgängig zu machen       (NOCH NICHT GETESTET)
+    def rollback(self):
+        self.connection.rollback()
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
+        
 
 
 
