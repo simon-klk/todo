9cca496b0095bbe5117929d6c863cd001b81f9e7
Simon <simon.kalka@outlook.de>
2024-12-20 09:42:52 +0100

    Daten Bank anbindung für Items
    Daten Bank anbindung für Items

--- main_gui.py
+++ main_gui.py
@@ -13,21 +13,24 @@ db = sqliteDB(sqlite3.connect("Listen.db"))
 db.create_DB_and_Tables()
 db.commit()
 
+# db.cursor.execute("INSERT INTO items (item_text, list_ID, item_beschreibung, item_datum, item_end_datum) VALUES ('name',2, 'beschreibung', '2021-1-1', '2021-1-1')")
+# db.commit()
 
-db.cursor.execute("SELECT list_name FROM listen")
-list_names = db.cursor.fetchall()
+# db.cursor.execute("SELECT list_name FROM listen")
+# list_names = db.cursor.fetchall()
 
-db.cursor.execute("SELECT list_ID FROM listen")
-list_IDs = db.cursor.fetchall()
+# db.cursor.execute("SELECT list_ID FROM listen")
+# list_IDs = db.cursor.fetchall()
 
-# Print the results
-for list_name in list_names:
-    print(list_name)
+db.cursor.execute("SELECT * FROM listen")
+lists = db.cursor.fetchall()
+print(lists[1][1])    
 
-# Convert the fetched list names to a list of strings
-listes = [name[0] for name in list_names]
 
-todos = ["Schuhe putzen", "Rainer telefonieren", "Banane", "test1", "test2", "test3", ]
+
+
+
+# todos = ["Schuhe putzen", "Rainer telefonieren", "Banane", "test1", "test2", "test3", ]
 
 
 
@@ -48,8 +51,9 @@ myfont = tk.CTkFont(family="Arial", size=16)
 """Buttons - oben"""
 # Open List Button
 
-def open_list(list_id):
-    todo_list = ToDoList(main, listes[list_id])
+def open_list(list_name):
+    todo_list = ToDoList(main, list_name)
+    print(list_name)
     todo_list.import_todo()
 
 
@@ -64,13 +68,13 @@ new_list_button.place(x=790, y=10)
 # Listen anzeigen
 def show_list():
     def on_click(list_name):
-        open_list(listes.index(list_name))
-        print(listes.index(list_name))
+        open_list(list_name)
+        
 
     list_buttons = []
     i = 10
-    for list_name in listes:    
-        list_button = tk.CTkButton(top_frame, text=list_name, width=90, height=40, command=lambda name=list_name: on_click(name))
+    for list in lists:    
+        list_button = tk.CTkButton(top_frame, text=list[1], width=90, height=40, command=lambda name=list: on_click(name))
         list_button.place(x=i, y=10)
         list_buttons.append(list_button)
         i += 100
@@ -78,19 +82,25 @@ def show_list():
     
 """Liste der ToDos - Mitte"""
 class ToDoList:
-    def __init__(self, master, name):
+    def __init__(self, master, list):
         self.master = master
         self.frame = tk.CTkFrame(self.master, width=892, height=480)
         self.frame.place(x=5, y=70)
-        self.label = tk.CTkLabel(self.frame, text=name, font=myfont, width=100, height=40)
+        self.label = tk.CTkLabel(self.frame, text=list[1], font=myfont, width=100, height=40)
         self.label.place(x=0, y=0)
+        
+        self.list_ID = list[0]
 
     
     def import_todo(self):
         new_x = 5
         new_y = 50
         i = 0
+        db.cursor.execute("SELECT * FROM items WHERE list_ID = ?", (self.list_ID,))
+        todos = db.cursor.fetchall()
         for todo in todos: 
+            
+
             ToDoFrame(master=self.frame, x=new_x, y=new_y, todo=todo)
             new_x += 180
             i += 1
